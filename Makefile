8a1fc6fa993689b2db0f0dc62329052e2ba28ef6
Mathias Schmitt <mathiaspeterhorst@gmail.com>
2020-08-24 19:58:28 +0200

    [DEV] maclookup: Check if mac is valid
    Makefile: Compile, install, clean, etc...
    Copying: added license

---
+++ Makefile
@@ -0,0 +1,81 @@
+# maclookup Find the manufacturer of a network interface
+# Copyright (C) 2020  Mathias Schmitt
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+COMPILER := gcc
+MATH_FLAGS := -lm
+ERROR_FLAGS := -Wall \
+				-Werror \
+				-Wextra \
+				-pedantic \
+				-Wshadow \
+				-Wdouble-promotion \
+				-fno-common \
+				-Wconversion
+FORMAT_FLAGS := -Wformat=2 -Wundef
+OPTIMIZATION_FLAGS := -O3
+FLAGS := ${MATH_FLAGS} ${ERROR_FLAGS} ${FORMAT_FLAGS} ${OPTIMIZATION_FLAGS}
+
+OUTPUT_DIR := out
+PROG_NAME := maclookup
+SRC := main.c
+
+MAN_DIR := man
+MAN := ${PROG_NAME}.1
+INSTALL_DIR := /usr/local/bin
+INSTALL_MAN_DIR := /usr/local/share/man/man1
+INSTALL_MAN_DIR_FR := /usr/local/share/man/fr/man1
+
+.PHONY: all
+all: maclookup
+
+.PHONY: maclookup
+maclookup: ${SRC}
+	@mkdir -p ${OUTPUT_DIR}
+	@${COMPILER} main.c ${FLAGS} -o ${OUTPUT_DIR}/${PROG_NAME}
+
+.PHONY: install
+install: ${OUTPUT_DIR}/${PROG_NAME} install_man
+	@mkdir -p ${DESTDIR}${INSTALL_DIR}
+	@cp $< ${DESTDIR}${INSTALL_DIR}/${PROG_NAME}
+
+.PHONY: uninstall
+uninstall: uninstall_man
+	@rm ${DESTDIR}${INSTALL_DIR}/${PROG_NAME}
+
+.PHONY: install_man
+install_man:
+	@mkdir -p ${DESTDIR}${INSTALL_MAN_DIR}
+	@cp ${MAN_DIR}/${MAN} ${DESTDIR}${INSTALL_MAN_DIR}/${MAN}
+
+	@mkdir -p ${DESTDIR}${INSTALL_MAN_DIR_FR}
+	@cp ${MAN_DIR}/fr/${MAN} ${DESTDIR}${INSTALL_MAN_DIR_FR}/${MAN}
+
+.PHONY: uninstall_man
+uninstall_man:
+	rm ${DESTDIR}${INSTALL_MAN_DIR}/${MAN}
+	rm ${DESTDIR}${INSTALL_MAN_DIR_FR}/${MAN}
+
+.PHONY: clean
+clean:
+	@rm -rf ${OUTPUT_DIR}
+
+.PHONY: help
+help:
+	@echo "Use one of the following targets:"
+	@echo "  help     Print this help message"
+	@echo "  all      Build maclookup"
+	@echo "  clean    Clean output from previous build"
+	@echo "  install  Install maclookup on your system"
---
+++ main.c
@@ -0,0 +1,194 @@
+/*
+    maclookup Find the manufacturer of a network interface
+    Copyright (C) 2020  Mathias Schmitt
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+#include <stdio.h>
+#include <errno.h>
+#include <getopt.h>
+#include <string.h>
+#include <ctype.h>
+#include <stdbool.h>
+
+#define OUI "oui.txt"
+#define OUI_PATH "./"
+
+#define MAC_LENGTH 17
+
+enum arguments {
+	ARGS_U = 0X01 /* Update the database of manufacturers */
+};
+
+static void usage(void)
+{
+	printf(
+		"Usage: maclookup [OPTIONS] [MAC-ADDRESS]\n"
+		"Display the manufacturer of a network interface\n"
+		"  -h  --help       display this help message and exit\n"
+		"  -u  --update     update the database of manufacturers\n"
+		"  -v  --version    output version information and exit\n");
+}
+
+static void version(void)
+{
+	printf(
+	"maclookup 1.0.0\n"
+	"\n"
+	"Copyright (C) 2020 Mathias Schmitt\n"
+	"License GPLv3+: GNU GPL version 3 or later <https://gnu.org/licenses/gpl.html>.\n"
+	"This is free software, and you are welcome to change and redistribute it\n"
+	"This program comes with ABSOLUTELY NO WARRANTY.\n");
+}
+
+static bool is_hexa(char c)
+{
+	int i;
+	char xdigit[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+		'a', 'b', 'c', 'd', 'e', 'f',
+		'A', 'B', 'C', 'D', 'E', 'F'};
+
+	for (i = 0; i < 22; ++i)
+		if (c == xdigit[i])
+			return true;
+
+	return false;
+}
+
+static bool is_separator(char c)
+{
+	static char previous_separator = '\0';
+	char separators[] = {':', '-', ' '};
+	int i;
+
+	for (i = 0; i < 3; ++i) {
+		if (c == separators[i]) {
+			if (previous_separator != '\0' && c != previous_separator) {
+				printf("Inconsistent use of '%c' and '%c' as a separator\n", c,
+					previous_separator);
+				return false;
+			}
+			previous_separator = c;
+			break;
+		}
+	}
+
+	return !(i >= 3);
+}
+
+static int mac_address_is_valid(char * mac)
+{
+	size_t length, i;
+	int counter = 0;
+	bool res = true;
+
+	length = strlen(mac);
+	if (length != MAC_LENGTH)
+		return res;
+
+	for (i = 0; i < length; ++i) {
+		if (counter == 2) {
+			counter = 0;
+			if (!is_separator(mac[i])) {
+				res = false;
+				break;
+			}
+		} else if (is_hexa(mac[i])) {
+			counter += 1;
+		} else {
+			res = false;
+			break;
+		}
+	}
+
+	return res;
+}
+
+static char check_arguments(int argc, char *argv[], char ** mac_address)
+{
+	int opt;
+	char args = 0;
+	struct option long_options[] = {
+		{"help", no_argument, 0, 'h'},
+		{"update", no_argument, 0, 'u'},
+		{"version", no_argument, 0, 'v'},
+		{0, 0, 0, 0}
+	};
+
+	while ((opt = getopt_long(argc, argv, "huv", long_options, NULL)) != -1) {
+		switch (opt) {
+		case 'u':
+			args |= ARGS_U;
+			break;
+		case 'v':
+			version();
+			return -EINVAL;
+		case 'h':
+		case '?':
+			usage();
+			return -EINVAL;
+		default:
+			break;
+		}
+	}
+
+	if (optind + 1 == argc) {
+		if (!mac_address_is_valid(argv[1])) {
+		printf("Invalid mac address: %s\n", argv[1]);
+			return -EINVAL;
+		}
+		printf("Valid mac address: %s\n", argv[1]);
+		*mac_address = strndup(argv[1], strlen(argv[1]));
+	} else if (optind < argc) {
+		printf("Invalid argument: %s\n", argv[1]);
+		usage();
+		return -EINVAL;
+	} else if (optind == argc) {
+		printf("Missing argument: mac address\n");
+		usage();
+		return -EINVAL;
+	}
+
+	return args;
+}
+
+static void update(void)
+{
+}
+
+static void find_manufacturer(void)
+{
+}
+
+int main(int argc, char *argv[])
+{
+	char *mac_address;
+	char args = 0;
+	int res = 0;
+
+	args = check_arguments(argc, argv, &mac_address);
+	if (args < 0) {
+		res = -EINVAL;
+		goto end;
+	}
+
+	if (args & ARGS_U)
+		update();
+	else
+		find_manufacturer();
+
+end:
+	return res;
+}
